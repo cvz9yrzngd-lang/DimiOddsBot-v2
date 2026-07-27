7b7e419152f715300bc700a1cd3fd86bfcfcd613
cvz9yrzngd-lang <cvz9yrzngd@privaterelay.appleid.com>
2026-07-27 07:20:33 +0100

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,42 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
+
+from database import add_user
+
+
+class TelegramBot:
+
+    def __init__(self, token):
+        self.app = Application.builder().token(token).build()
+
+        self.app.add_handler(
+            CommandHandler("start", self.start)
+        )
+
+    async def start(
+        self,
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+    ):
+        add_user(update.effective_chat.id)
+
+        await update.message.reply_text(
+            "✅ DimiOddsBot v2 е стартиран."
+        )
+
+    async def send_alert(self, text):
+
+        from database import get_users
+
+        for chat_id in get_users():
+            try:
+                await self.app.bot.send_message(
+                    chat_id=chat_id,
+                    text=text,
+                )
+            except Exception:
+                pass
